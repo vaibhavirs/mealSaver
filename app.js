e071e8545407b653d0087fe6c8d6440f1f7b9038
Sahil Ghorpade <sahilpg27@gmail.com>
2024-10-25 00:11:17 +0530

    login and registration page added

--- app.js
+++ app.js
@@ -13,5 +13,13 @@ app.get("/",(req,res)=>{
     res.render("index");
 });
 
+app.get("/register", (req,res)=>{
+    res.render("register");
+})
+
+app.get("/login", (req,res)=>{
+    res.render("login");
+})
+
 
 app.listen(3000);
