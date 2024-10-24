9da1124ff486b5344d75f3f61a8954b064d97181
Sahil Ghorpade <sahilpg27@gmail.com>
2024-10-24 12:30:32 +0530

    added some more packages

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express")
 const app = express();
 const path = require("path");
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname , 'public')));
+
 
 app.get("/",(req,res)=>{
-    res.send("hey");
-})
+    res.render("index");
+});
+
 
 app.listen(3000);
